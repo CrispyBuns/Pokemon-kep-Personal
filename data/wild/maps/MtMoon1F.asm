2745b6cbba879f3bc157e4e2723c470b656b5069
CrispyBuns <Crispybuns2003@gmail.com>
2026-04-29 18:36:49 -0500

    Revert "Add Magikarp to Mt Moon Encounters"
    This reverts commit c98b1b09c3edb230513c6f565f4528199240c6e4.

--- data/wild/maps/MtMoon1F.asm
+++ data/wild/maps/MtMoon1F.asm
@@ -1,15 +1,15 @@
 MtMoon1FWildMons:
-	def_grass_wildmons 11 ; Encounter Rate
-	db  8, MAGIKARP
-	db  7, MAGIKARP
+	def_grass_wildmons 10 ; encounter rate
+	db  8, BITTYBAT
+	db  7, BITTYBAT
 	db  9, BITTYBAT
 	db  8, GEODUDE
-	db  6, PARAS
+	db  6, BITTYBAT
 	db  9, SANDSHREW
 	db  9, GEODUDE
 	db  8, PARAS
 	db 7, GEODUDE
-	db  8, MAGIKARP
+	db  8, GEODUDE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
