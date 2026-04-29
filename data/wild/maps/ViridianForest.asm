c4d0bc8a3ab98c93335e12a2506e153c914d71cb
CrispyBuns <Crispybuns2003@gmail.com>
2026-04-29 15:47:16 -0500

    adjust Pokemon encounters to my liking

--- data/wild/maps/ViridianForest.asm
+++ data/wild/maps/ViridianForest.asm
@@ -1,11 +1,11 @@
 ViridianForestWildMons:
-	def_grass_wildmons 8 ; encounter rate
+	def_grass_wildmons 12 ; encounter rate
 	db  6, KOTORA
 	db  5, KOTORA
 	db  4, PIDGEY
-	db  6, BLASTYKE
-	db  6, BLASTYKE	
-	db  5, BLASTYKE	
+	db  6, MAGIKARP
+	db  6, MAGIKARP	
+	db  5, MAGIKARP
 	db  7, KOTORA
 	db  7, METAPOD
 	db  5, KOTORA ; A replacement for the early starter Pikachu. I think this is an amazing compromise that helps actualise Kotora's "mascot status" when it comes to prototype Pokemon.
