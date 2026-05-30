0d14ad098f69e2ad2839a970ccc00acc7ea483e3
CrispyBuns <Crispybuns2003@gmail.com>
2026-05-30 15:30:39 -0500

    Graveler can now learn Mega Kick

--- data/pokemon/base_stats/graveler.asm
+++ data/pokemon/base_stats/graveler.asm
@@ -15,7 +15,7 @@
 
 	; tm/hm learnset
 	tmhm MEGA_PUNCH,   TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  \
-	     SUBMISSION,   COUNTER,      SEISMIC_TOSS, RAGE,         EARTHQUAKE,   \
+	     SUBMISSION,   COUNTER,      MEGA_KICK, RAGE,         EARTHQUAKE,   \
 	     FISSURE,      DIG,          MIMIC,        DOUBLE_TEAM,  BIDE,         \
 	     METRONOME,    SELFDESTRUCT, FIRE_BLAST,   REST,         EXPLOSION,    \
 	     ROCK_SLIDE,   SUBSTITUTE,   STRENGTH
